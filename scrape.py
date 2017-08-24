de4079dcefb54b7457f9623b3ad928394306a10c
SharanKuamr95 <ssharankumar95@gmail.com>
2017-08-24 17:36:45 +0530

    Gets info from tables in websites and stores it in a file. The file can be opened in Excel

--- scrape.py
+++ scrape.py
@@ -1,1 +1,24 @@
-print "hello world"
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'https://en.wikipedia.org/wiki/Resource_Reservation_Protocol'
+response = requests.get(url)
+html = response.content
+
+soup=BeautifulSoup(html)
+table=soup.find('table',attrs={'class':'vertical-navbox nowraplinks hlist'})
+list_of_rows=[]
+for row in table.findAll('tr')[1:]:
+    list_of_cells=[]
+    for cell in row.findAll('a'):
+         # print row.prettify()
+         text= cell.text
+         list_of_cells.append(text)
+    list_of_rows.append(list_of_cells)
+#print list_of_rows
+
+outfile=open("./inmates.csv","wb")
+writer= csv.writer(outfile)
+writer.writerow(["Last"])
+writer.writerows(list_of_rows)
